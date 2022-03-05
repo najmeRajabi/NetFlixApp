7ce6c696f2c05f568acd7a296a5ed29d48617044
niha <niha8515@yahoo.com>
2022-03-05 05:03:24 -0800

    add video_layout.xml design fragment_home.xml rename fragment_profile.xml

--- app/src/main/java/com/example/netflixapp/ProfileFragment.kt
+++ app/src/main/java/com/example/netflixapp/ProfileFragment.kt
@@ -20,7 +20,7 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_proflix, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 
