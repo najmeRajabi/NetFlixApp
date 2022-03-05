77554973d0a5c03e78befd5691db2c85d34dcffc
niha <niha8515@yahoo.com>
2022-03-05 03:42:46 -0800

    change menu to home fragment add findNavController to menuItems

--- app/src/main/java/com/example/netflixapp/HomeFragment.kt
+++ app/src/main/java/com/example/netflixapp/HomeFragment.kt
@@ -1,16 +1,18 @@
 package com.example.netflixapp
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 
 class HomeFragment : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
 
     }
@@ -23,5 +25,35 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_bar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.favorite_item_menu -> {
+
+                Toast.makeText(activity,"fave", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_homeFragment_to_favoriteFragment)
+                true
+            }
+            R.id.comingSoon_item_menu -> {
+                Toast.makeText(activity,"came", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_homeFragment_to_comingSoonFragment)
+                true
+            }
+            R.id.home_item_menu -> {
+                Toast.makeText(activity,"home", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.profile_item_menu -> {
+                Toast.makeText(activity,"profile", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
--- app/src/main/java/com/example/netflixapp/MainActivity.kt
+++ app/src/main/java/com/example/netflixapp/MainActivity.kt
@@ -4,15 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.menu_bar, menu)
-        return true
-    }
+
 }
