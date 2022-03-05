47b9942922ffe0f87ee89563f5749aabbc1489f4
niha <niha8515@yahoo.com>
2022-03-05 02:43:17 -0800

    Add menu

--- app/src/main/java/com/example/netflixapp/MainActivity.kt
+++ app/src/main/java/com/example/netflixapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.netflixapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_bar, menu)
+        return true
     }
 }
