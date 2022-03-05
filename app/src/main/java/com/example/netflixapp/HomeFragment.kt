a0caa890cf9d8ae09fd3c7cc55ef9350ee9fed69
niha <niha8515@yahoo.com>
2022-03-05 08:58:16 -0800

    add icon show image and text in home

--- app/src/main/java/com/example/netflixapp/HomeFragment.kt
+++ app/src/main/java/com/example/netflixapp/HomeFragment.kt
@@ -1,28 +1,56 @@
 package com.example.netflixapp
 
+import android.annotation.SuppressLint
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import android.widget.Toast.makeText
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+
+import com.example.netflixapp.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
 
+    lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
 
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        val boxList = arrayListOf(binding.listVideo1,binding.listVideo2,
+        binding.listVideo3,binding.listVideo4,binding.listVideo5,binding.listVideo6,
+            binding.listVideo7,binding.listVideo8,binding.listVideo9,
+            binding.listVideo10,binding.listVideo11,binding.listVideo12,
+            binding.listVideo13,binding.listVideo14,binding.listVideo15,
+            binding.listVideo16,binding.listVideo17, binding.listVideo18,
+            binding.listVideo19,binding.listVideo20,binding.listVideo21)
+
+        binding.listVideo1.txvVideoImage.text = Videos.videos[0].name
+        binding.listVideo1.videoImage.setImageResource(Videos.videos[0].image)
+
+        for (i in 0 until boxList.size){
+            boxList[i].txvVideoImage.text = Videos.videos[i].name
+            boxList[i].videoImage.setImageResource(Videos.videos[i].image)
+            if (Videos.videos[i].isFave){
+                boxList[i].imvFav.setColorFilter(R.color.red,PorterDuff.Mode.LIGHTEN)
+//                boxList[i].imvFav.setImageResource(R.drawable.ic_baseline_favorite_24)
+            }
+        }
+
+
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
