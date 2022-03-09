bb00b3847ab2aa72b92c67d3194c03c7a5b8ef39
niha8515 <niha8515@yahoo.com>
2022-03-09 14:31:49 +0330

    delete menu item home

--- app/src/main/java/com/example/netflixapp/HomeFragment.kt
+++ app/src/main/java/com/example/netflixapp/HomeFragment.kt
@@ -15,14 +15,14 @@ import com.example.netflixapp.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
 
-    lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
         super.onCreate(savedInstanceState)
 
     }
 
-//    @SuppressLint("ResourceAsColor")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,19 +32,15 @@ class HomeFragment : Fragment() {
         binding.listVideo3,binding.listVideo4,binding.listVideo5,binding.listVideo6,
             binding.listVideo7,binding.listVideo8,binding.listVideo9,
             binding.listVideo10,binding.listVideo11,binding.listVideo12)
-//            binding.listVideo13,binding.listVideo14,binding.listVideo15,
-//            binding.listVideo16,binding.listVideo17, binding.listVideo18,
-//            binding.listVideo19,binding.listVideo20,binding.listVideo21)
 
-//        binding.listVideo1.txvVideoImage.text = Videos.videos[0].name
-//        binding.listVideo1.videoImage.setImageResource(Videos.videos[0].image)
+
+
 
         for (i in 0 until boxList.size){
             boxList[i].txvVideoImage.text = Videos.videos[i].name
 //            boxList[i].videoImage.setImageResource(Videos.videos[i].image)
             boxList[i].videoImage.setImageResource(R.drawable.f9 )
             if (Videos.videos[i].isFave){
-//                boxList[i].imvFav.setBackgroundResource(R.drawable.ic_baseline_favorite_red)
                 boxList[i].imvFav.setImageResource(R.drawable.ic_baseline_favorite_red)
             }
         }
@@ -70,10 +66,6 @@ class HomeFragment : Fragment() {
             R.id.comingSoon_item_menu -> {
                 Toast.makeText(activity,"came", Toast.LENGTH_SHORT).show()
                 findNavController().navigate(R.id.action_homeFragment_to_comingSoonFragment)
-                true
-            }
-            R.id.home_item_menu -> {
-                Toast.makeText(activity,"home", Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.profile_item_menu -> {
