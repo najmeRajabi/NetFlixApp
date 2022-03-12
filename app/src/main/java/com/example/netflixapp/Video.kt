845cfef35c384e01d4ada89ca54afdaf3c490768
niha8515 <niha8515@yahoo.com>
2022-03-12 10:46:41 +0330

    make all favorite false for defult

--- app/src/main/java/com/example/netflixapp/Video.kt
+++ app/src/main/java/com/example/netflixapp/Video.kt
@@ -6,15 +6,15 @@ object Videos{
     val videos = arrayListOf<Video>(
         Video("2021",false,R.drawable.f9),
         Video("Crisis",false,R.drawable.f9),
-        Video("Dune",true,R.drawable.f9),
+        Video("Dune",false,R.drawable.f9),
         Video("Lamb",false,R.drawable.f9),
         Video("Masquerade",false,R.drawable.f9),
-        Video("Oxygen",true,R.drawable.f9),
+        Video("Oxygen",false,R.drawable.f9),
         Video("Reminiscence",false,R.drawable.f9),
         Video("Settlers",false,R.drawable.f9),
         Video("The Colony",false,R.drawable.f9),
         Video("Warning",false,R.drawable.f9),
-        Video("fast & furious9",true,R.drawable.f9),
+        Video("fast & furious9",false,R.drawable.f9),
         Video("land",false,R.drawable.f9)
 
     )
